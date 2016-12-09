401e723969bb5651c5ef2d5e9004f7b8a7be6278
hungjie19 <hungjie19@gmail.com>
2016-12-09 13:44:50 +0800

    Day 03 - 一定要寫個 Hello word 與雙向綁定

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="hello">
+  
+    <!-- 2. msg 雙向綁定到 template 的地方 -->
     <h1>{{ msg }}</h1>
+    
+    <!-- 4. 插入我們新增的數據看看，兩個 {{ }} 中可以綁定 data 任意值 -->
+    <h2>{{ hello }}</h2>
+    
+    <!-- 5. 也可以撰寫一些 javascript code -->
+    <h2>{{ hello + ' and Ironman 2017' }}</h2>
+
+    <!-- 6. 加一個 input 來嘗試今天的任務，雙向綁定 
+         修改 input 裡面的值，綁定的地方也會馬上更新!!
+         是不是很酷呢?! -->
+    <input type="text" v-model="hello" />
+    
+    <!-- 8. 使用 checkbox 來完成雙向綁定 -->
+    <br>
+    <input id="toggle" type="checkbox" v-model="toggle" />
+    <label for="toggle">{{ toggle }}</label>
+    
+    <!-- 以上今天範例 -->
+
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -25,7 +46,15 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      // 1. data 這裡就是 model 存放一些資料
+      // 這邊的資料與 tmplate 可以做雙向綁定
+      msg: 'Welcome to Your Vue.js App',
+      
+      // 3. 嘗試新增一個數據
+      hello: 'Hello Vue 2.0 !',
+      
+      // 7. checkbox 預設是 false
+      toggle: false,
     }
   }
 }
