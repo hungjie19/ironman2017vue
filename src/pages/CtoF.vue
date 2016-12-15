71aa117fa0a950e569aee2571a991ed3c36cbd14
hungjie19 <hungjie19@gmail.com>
2016-12-15 12:04:00 +0800

    vue & vuex 06 - 攝氏轉換華氏工具 - II (v-if, v-show, methods)

--- src/pages/CtoF.vue
+++ src/pages/CtoF.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="container">
-    <!-- 公式撰寫在 template -->
-    <h2>華氏：{{ celsius * 9/5 + 32 }} °F</h2>
-    <!-- 公式收納在 computed -->
-    <h2>華氏：{{ fahrenheit }} °F</h2>
+    <h1>Temperature Conversion</h1>
+    <!-- 切換模式按鈕 -->
+    <input id="toggleTemperature" type="checkbox" v-model="isCelsius">
+    <label for="toggleTemperature">切換：{{ toggleTemperature }}</label>
+    
+    <!-- 轉換顯示區域 -->
+    <!-- v-if 條件不成立的區域，不會存在瀏覽器上 -->
+    <h2 v-if="isCelsius">攝氏：{{ celsius }} °C</h2>
+    <h2 v-if="!isCelsius">華氏：{{ fahrenheit }} °F</h2>
+    
+    <!-- 使用者輸入區域 -->
+    <!-- v-show 條件不成立的區域會使用 style 隱藏，會存在瀏覽器上 -->
     <div class="celsius">
-      攝氏：<input type='number' v-model="celsius" /> °C
+      <span v-show="!isCelsius">攝氏：</span>
+      <span v-show="isCelsius">華氏：</span>
+      <input type='number' v-model="userInput" /> °C
     </div>
+
+    <!-- alert 顯示計算結果 -->
+    <button @click="temperatureConversio( userInput );">
+      Temperature Conversio
+    </button>
   </div>
 </template>
 
@@ -14,19 +29,34 @@
 export default {
   data () {
     return {
-      celsius: 0
+      userInput: 0,
+      // 增加此變數紀錄目前是什麼模式。
+      isCelsius: false,
     }
   },
   computed: {
-    // ES6:
     fahrenheit () {
-      // computed 計算後才會顯示到 template 上面。
-      return this.celsius * 9/5 + 32;
+      return this.userInput * 9/5 + 32;
+    },
+    celsius () {
+      return (this.userInput - 32) * 5/9;
+    },
+    toggleTemperature () {
+      return this.isCelsius ? "攝氏 轉換 華氏" : "華氏 轉換 攝氏";
     }
-    // ES5:
-    // fahrenheit: function () {
-    //   return this.celsius * 9/5 + 32;
-    // }
+  },
+  methods: {
+    temperatureConversio ( val ) {
+      // val 是從 click 傳進來的 userInput 這是示範 function 傳值。
+      // 如果不傳值，直接使用 this.userInput 也可以。
+      // alert 的內容直接使用 computed 已經幫我計算好的結果。
+      if ( this.isCelsius ) {
+        alert('攝氏: ' + this.celsius );
+      } 
+      else {
+        alert('華氏: ' + this.fahrenheit );
+      }
+    }
   }
 }
 </script>
@@ -34,5 +64,6 @@ export default {
 <style>
   .celsius {
     font-size: 1.5em;
+    margin-bottom: 1em;
   }
 </style>
