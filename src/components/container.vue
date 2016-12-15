d8da887a552d57fe3f2e646db0f614cdee675ccc
hungjie19 <hungjie19@gmail.com>
2016-12-15 16:26:59 +0800

    vue & vuex 08 - component - II (v-for 小幫手解決重複元件) -1
    - v-for 整理資料

--- src/components/container.vue
+++ src/components/container.vue
@@ -2,20 +2,10 @@
   <div class="container">
     <!-- Example row of columns -->
     <div class="row">
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-     </div>
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+      <div class="col-md-4" v-for="item in list">
+        <h2>{{ item.title }}</h2>
+        <p>{{ item.info }}</p>
+        <p><a class="btn btn-default" href="#" role="button">{{ item.btntext }}</a></p>
       </div>
     </div>
 
@@ -36,7 +26,29 @@
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+      list: [
+        {
+          title: 'Heading',
+          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+          btntext: 'View details »'
+        },
+        {
+          title: 'Heading',
+          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+          btntext: 'View details »'
+        },
+        {
+          title: 'Heading',
+          info: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+          btntext: 'View details »'
+        },
+      ],
+    }
+  },
+}
 </script>
 
 <style>
