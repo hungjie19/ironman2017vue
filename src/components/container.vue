ed014731e9a1bc629619e9a79e51f1b2935a6fbc
hungjie19 <hungjie19@gmail.com>
2016-12-15 16:31:59 +0800

    vue & vuex 08 - component - II - use props

--- src/components/container.vue
+++ src/components/container.vue
@@ -27,27 +27,11 @@
 
 <script>
 export default {
-  data () {
-    return {
-      list: [
-        {
-          title: 'Heading',
-          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
-          btntext: 'View details »'
-        },
-        {
-          title: 'Heading',
-          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
-          btntext: 'View details »'
-        },
-        {
-          title: 'Heading',
-          info: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
-          btntext: 'View details »'
-        },
-      ],
-    }
-  },
+  // vue option 當中有 props 開發區域
+  props: {
+    // attribute name: Type
+    list: Array
+  }, 
 }
 </script>
 
--- src/pages/learnComponent.vue
+++ src/pages/learnComponent.vue
@@ -2,7 +2,7 @@
   <div id="jumbotron">
     <nav-bar/>
     <jumbotron />
-    <container />
+    <container :list="list"/>
   </div>
 </template>
 
@@ -16,7 +16,28 @@ export default {
     NavBar,
     Jumbotron,
     Container
-  }
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: 'Heading',
+          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+          btntext: 'View details »'
+        },
+        {
+          title: 'Heading',
+          info: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. ',
+          btntext: 'View details »'
+        },
+        {
+          title: 'Heading',
+          info: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.',
+          btntext: 'View details »'
+        },
+      ],
+    }
+  },
 }
 </script>
 
