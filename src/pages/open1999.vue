86eadee198b9283dc7886c335d21fe06d4619b10
hungjie19 <hungjie19@gmail.com>
2016-12-24 11:08:38 +0800

    vue & vuex 23 - Open Data - III (搜尋條件、select option、v-else、watch)

--- src/pages/open1999.vue
+++ src/pages/open1999.vue
@@ -5,6 +5,36 @@
         <div class="navbar-header">
           <a class="navbar-brand" href="#">open1999</a>
         </div>
+        <div class="navbar-form navbar-left">
+          <div class="form-group">
+            <label for="searchRegion">搜尋地區：</label>
+            <!-- 
+              在下拉式選單中 `select` tag 上使用 v-model 
+              `option` 的選擇將會是 model 的 value 
+            -->
+            <select id="searchRegion"class="form-control" v-model="searchRegion">
+              <option value='all'>全部</option>
+              <option v-for="option in regionOption" :value="option">
+                {{ option }}
+              </option>
+            </select>
+          </div>
+          <div class="input-group">
+            <input 
+              type="text"
+              class="form-control"
+              placeholder="Search"
+              v-model="searchKeyword"
+              @keyup.enter="handleWorkSearch">
+            <div class="input-group-btn">
+              <button class="btn btn-default" @click="handleWorkSearch">
+                <i class="glyphicon glyphicon-search"></i>
+              </button>
+            </div>
+          </div>
+
+        </div>
+
         <ul class="nav navbar-nav navbar-right">
           <li class="nav-item">
             <a class="nav-link hidden-xs-down" target="_blank" href="http://data.kcg.gov.tw/dataset/open1999">資料出處：data.kcg.gov.tw</a>
@@ -12,26 +42,39 @@
         </ul>
       </div>
     </nav>
-    
+  
     <hr>
-    
-    <!-- card -->
-    <div class="row">
-      <div class="col-md-4 col-sm-6" v-for="item in opendate">
-        <div class="thumbnail">
-          <span class="label label-warning">{{ item.ZipName_ }}</span>
-          <span class="label label-ligth-pink">{{ item.InformDesc_ }}</span>
-          <span class="label label-info">{{ item.UnitName_ }}</span>
-          <div class="caption">
-            <h3>
-              <a :href="getGoogleMap( item.address_ )" target="_blank">{{ item.address_ }}</a>
-            </h3>
-            <p>{{ item.BeforeDesc_ }}</p>
-            <small class="text-muted">反應日期：{{ item.Cre_Date_ }}</small>
+<!-- if -->
+    <div v-if="opendate.length===0" class="alert alert-info" role="alert">
+      很抱歉！找不要您到的資訊。
+    </div>
+<!-- else 
+      除了使用"!" <div v-if="!opendate.length===0"> 
+      判斷反向數值之外，Vue 2.1 有 v-else, v-else-if directive 可以方便使用
+    -->
+    <div v-else>
+      <div class="alert alert-success" role="alert">
+        總共查到：{{ opendate.length }} 筆資料。
+      </div>
+      <!-- card -->
+      <div class="row">
+        <div class="col-md-4 col-sm-6" v-for="item in opendate">
+          <div class="thumbnail">
+            <span class="label label-warning">{{ item.ZipName_ }}</span>
+            <span class="label label-ligth-pink">{{ item.InformDesc_ }}</span>
+            <span class="label label-info">{{ item.UnitName_ }}</span>
+            <div class="caption">
+              <h3>
+                <a :href="getGoogleMap( item.address_ )" target="_blank">{{ item.address_ }}</a>
+              </h3>
+              <p>{{ item.BeforeDesc_ }}</p>
+              <small class="text-muted">反應日期：{{ item.Cre_Date_ }}</small>
+            </div>
           </div>
         </div>
       </div>
     </div>
+<!-- end eles -->
 
   </div>
 </template>
@@ -40,12 +83,19 @@
 import { mapGetters, mapActions } from 'vuex';
 
 export default {
+  data() {
+    return {
+      searchRegion: 'all',
+      searchKeyword: '',
+    }
+  },
   created () {
     // created Hook 發出 action call API
     this.$store.dispatch('open1999');
   },
   computed: mapGetters({
-    opendate: 'getOpen1999'
+    opendate: 'getOpen1999',
+    regionOption: 'getRegionOption'
   }),
   methods: {
     ...mapActions([
@@ -56,6 +106,22 @@ export default {
       // ES5: "https://www.google.com/maps/place/" + address;
       return `https://www.google.com/maps/place/${address}`;
     },
+    handleWorkSearch () {
+      this.$store.dispatch('opendataSearchKeyword', this.searchKeyword);
+    },
+  },
+  /*
+    當 select 改變時，會雙向綁定 option value
+    因此我們監聽 `searchRegion`（預設是 all）
+    將改變的 value 發出 action 改變 state
+    state 改變 getter 將會重新計算，因此我們得到新的搜尋結果。
+  */
+  watch: {
+    searchRegion (val) {
+      // val 是 searchRegion 的 value（打開下面 console 試試看）
+      // cosole.log('watch searchRegion?', searchRegion);
+      this.$store.dispatch('opendataSearchRegion', val);
+    }
   },
 };
 </script>
--- src/store/modules/opendata.js
+++ src/store/modules/opendata.js
@@ -3,14 +3,35 @@ import * as rootypes from '../mutations_types.js';
 
 const types = {
   OPEN_1999: 'open/OPEN_1999',
+  OPENDATA_SEARCH_REGION: 'open/OPENDATA_SEARCH_REGION',
+  OPENDATA_SEARCH_KEYWORD: 'OPENDATA_SEARCH_KEYWORD',
 }
 
 const state = {
   opendata: [],
+  RegionOption: ["三民區", "仁武區", "內門區", "六龜區", "前金區", "前鎮區", "大寮區", "大樹區", "大社區", "小港區", "岡山區", "左營區", "彌陀區", "新興區", "旗山區", "旗津區", "杉林區", "林園區", "桃源區", "梓官區", "楠梓區", "橋頭區", "永安區", "湖內區", "燕巢區", "田寮區", "甲仙區", "美濃區", "苓雅區", "茂林區", "茄萣區", "路竹區", "那瑪夏區", "阿蓮區", "鳥松區", "鳳山區", "鹽埕區", "鼓山區"],
+  search: {
+    region: 'all',
+    keyword: ''
+  }
 }
 
 const getters = {
-  getOpen1999: state => state.opendata,
+  getRegionOption: state => state.RegionOption,
+  getOpen1999: state => {
+    // 將原始資料儲存在另一變數下 `_opendata`，
+    let _opendata = state.opendata;
+    // 出現搜尋條件的時候，使用 filter 過濾條件，
+    if (state.search.region !== 'all') {
+      _opendata = _opendata.filter( item => (item.ZipName_ === state.search.region) );
+    }
+    // 出現搜尋條件的時候，使用 filter 過濾條件，
+    if (state.search.keyword !== '') {
+      _opendata = _opendata.filter( item => (JSON.stringify( item ).indexOf( state.search.keyword ) != -1) );
+    }
+    // 回傳結果。
+    return _opendata;
+  }
 }
 
 const actions = {
@@ -42,11 +63,28 @@ const actions = {
         commit(rootypes.LOADING, false);
       });
   },
+
+  opendataSearchRegion ({ commit }, region) {
+    console.log('opendataSearchRegion', region);
+    commit(types.OPENDATA_SEARCH_REGION, region);
+  },
+
+  opendataSearchKeyword ({ commit }, keyword) {
+    commit(types.OPENDATA_SEARCH_KEYWORD, keyword);
+  },
 }
 
 const mutations = {
   [types.OPEN_1999] (state, data) {
     state.opendata = data;
+  },
+
+  [types.OPENDATA_SEARCH_REGION] (state, region) {
+    state.search.region = region;
+  },
+
+  [types.OPENDATA_SEARCH_KEYWORD] (state, keyword) {
+    state.search.keyword = keyword;
   },
 }
 
